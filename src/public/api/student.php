c5e03216def073aef6f65d6a27d4d4d13d5a25c2
Stanislav Despotovic <stanislav@siposdoo.com>
2022-01-02 18:05:11 +0100

    typo

--- src/public/api/student.php
+++ src/public/api/student.php
@@ -2,7 +2,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     
-    include_once '../../config/database.php';
+    include_once '../../config/Database.php';
     include_once '../../class/Student.php';
     $database = new Database();
     $db = $database->getConnection();
